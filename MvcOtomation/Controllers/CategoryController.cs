8516094190cc3df75d843b5d8df0241c587f4734
Remzi Ozbek <Remzi.Ozbek@logo.com.tr>
2023-03-22 15:29:52 +0300

    Add Category, Migration

---
+++ MvcOtomation/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOtomation.Models.Classes;
+
+namespace MvcOtomation.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Index()
+        {
+            Context context = new Context();
+            var categories = context.Categories.ToList();
+            return View(categories);
+        }
+    }
+}
